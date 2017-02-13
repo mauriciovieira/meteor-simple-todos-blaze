c63f592277b9d61444a471cd7ae52febaf4a69b4
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-02-13 00:37:21 -0200

    Install and setup eslint

--- client/main.js
+++ client/main.js
@@ -1,9 +1,6 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.hello.onCreated(function () {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
@@ -15,8 +12,8 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
+  'click button': function (event, templateInstance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    templateInstance.counter.set(templateInstance.counter.get() + 1);
   },
 });
